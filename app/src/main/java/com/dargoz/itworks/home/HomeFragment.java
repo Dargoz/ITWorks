0c5fb0a15f0450f9e21819d491bfd3135213a5b8
Davin Reinaldo G <davinreinaldogozali@gmail.com>
2021-09-02 08:37:35 +0700

    make specific ui/ux section

--- app/src/main/java/com/dargoz/itworks/home/HomeFragment.java
+++ app/src/main/java/com/dargoz/itworks/home/HomeFragment.java
@@ -2,17 +2,14 @@ package com.dargoz.itworks.home;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.dargoz.itworks.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.dargoz.itworks.databinding.FragmentHomeBinding;
 import com.dargoz.itworks.uiux.UserInterfaceActivity;
 
@@ -39,9 +36,6 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.buttonListFragment.setOnClickListener(v ->
-                Navigation.findNavController(view).navigate(R.id.action_home_to_list_fragment)
-        );
 
         binding.buttonUiUx.setOnClickListener(v -> {
             Intent intent = new Intent(getContext(), UserInterfaceActivity.class);
